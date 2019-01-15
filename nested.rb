9c54d09edaf53db2e9b434d41bd05affe75ee562
JanCarlos Acevedo <jancarlos92@me.com>
2019-01-15 19:59:58 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -79,6 +79,10 @@ def adding_matz
     
 end
 
+programmer_hash = 
+ 		{:grace_hopper => {:known_for => "COBOL",:languages => ["COBOL", "FORTRAN"] }, :alan_kay =>{:known_for => "Object Orientation", :languages => ["Smalltalk", "LISP"]}, :dennis_ritchie => {:known_for => "Unix",:languages => ["C"]}
+     }
+
 def changing_alan
 	
 	
