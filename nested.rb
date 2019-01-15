8139fde4ae9864efcffcb6835c16086344722c8b
JanCarlos Acevedo <jancarlos92@me.com>
2019-01-15 19:45:27 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,16 +82,11 @@ def changing_alan
 	
 	
 	programmer_hash = 
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
+ 		{:grace_hopper => {:known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"] },:alan_kay => {
           :known_for => "Object Orientation",
           :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
+        }, :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
         }
